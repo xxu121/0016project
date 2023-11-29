2be53b6706b8819e17e2b874cae4e931afec3aa1
xxu121 <146341729+xxu121@users.noreply.github.com>
2023-11-29 22:39:21 +0000

    Update sketch_nov29bRIPtset.ino

--- sensors test/sketch_nov29bRIPtset_副本/sketch_nov29bRIPtset.ino
+++ sensors test/sketch_nov29bRIPtset_副本/sketch_nov29bRIPtset.ino
@@ -1,18 +1,39 @@
-// Define the pin where the RIP sensor is connected
-const int ripSensorPin = 10;
+/// Define the pin where the PIR sensor is connected
+const int pirSensorPin = 10;
+// The built-in LED on most Arduino boards is on pin 13
+const int ledPin = 13;
 
 void setup() {
   // Start serial communication at 9600 baud rate
   Serial.begin(9600);
+  
+  // Declare the PIR sensor pin as an INPUT
+  pinMode(pirSensorPin, INPUT);
+  
+  // Declare the LED pin as an OUTPUT
+  pinMode(ledPin, OUTPUT);
+  
+  Serial.println("PIR sensor test initialized.");
+  Serial.println("Waiting for PIR to stabilize.");
+  // Give the sensor some time to calibrate
+  delay(2000);
+  Serial.println("PIR sensor ready.");
 }
 
 void loop() {
-  // Read the value from the RIP sensor
-  int sensorValue = analogRead(ripSensorPin);
+  // Read the value from the PIR sensor
+  int sensorValue = digitalRead(pirSensorPin);
 
-  // Print the value to the Serial Monitor
-  Serial.println(sensorValue);
+  // If motion is detected (sensor pin goes HIGH), turn on the LED
+  if (sensorValue == HIGH) {
+    digitalWrite(ledPin, HIGH); // Turn on the LED
+    Serial.println("Motion detected!");
+  } else {
+    digitalWrite(ledPin, LOW); // Turn off the LED
+    // If you want to print a message when no motion is detected, uncomment the next line:
+    // Serial.println("No motion.");
+  }
 
-  // Wait for a short time before reading again
-  delay(1000); // Delay in milliseconds (100ms = 0.1s)
+
+  delay(1000);
 }
